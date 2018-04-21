3a1760c1379037dc8c4ad086eb33dbff0f931664
andy <andy@hurryout.com>
2018-04-21 13:02:04 -0700

    Targets. Initial commit

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+    public Material onMaterial;
+    public Material offMaterial;
+
+    void OnTriggerEnter(Collider other)
+    {
+        GetComponent<Renderer>().material = onMaterial;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        GetComponent<Renderer>().material = offMaterial;
+    }
+}
