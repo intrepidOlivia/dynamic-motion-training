97da78f702b170e6173e80214d24299db6749966
spayne <sean.d.payne@gmail.com>
2018-04-21 13:03:04 -0700

    Modify the TrackedObject to be aware of the knee and ankle roles.
    Use the serial numbers of the trackers to ensure that green is knee and blue is ankle.  The idea here is to avoid extra problems and confusion.

---
+++ Assets/DrawShin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawShin : MonoBehaviour {
+
+    public Transform knee;
+    public Transform ankle;
+    public LineRenderer liney;
+
+    Vector3[] positions;
+
+    // Use this for initialization
+    void Start () {
+		positions = new Vector3[2];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        positions[0] = knee.position;
+        positions[1] = ankle.position;
+
+       liney.GetComponent<LineRenderer>().SetPositions(positions);
+
+    }
+}
--- Assets/SteamVR/Scripts/SteamVR_TrackedObject.cs
+++ Assets/SteamVR/Scripts/SteamVR_TrackedObject.cs
@@ -9,96 +9,166 @@ using Valve.VR;
 
 public class SteamVR_TrackedObject : MonoBehaviour
 {
-	public enum EIndex
-	{
-		None = -1,
-		Hmd = (int)OpenVR.k_unTrackedDeviceIndex_Hmd,
-		Device1,
-		Device2,
-		Device3,
-		Device4,
-		Device5,
-		Device6,
-		Device7,
-		Device8,
-		Device9,
-		Device10,
-		Device11,
-		Device12,
-		Device13,
-		Device14,
-		Device15
-	}
 
-	public EIndex index;
+    private const int kMaxDevicesToSearch = 10;
+    private const string GreenTrackerSerial = "LHR-1EA69A06";
+    private const string BlueTrackerSerial = "LHR-1890594A";
 
-	[Tooltip("If not set, relative to parent")]
-	public Transform origin;
+    public enum EPhysioRole
+    {
+        None = 0,
+        Knee = 1,   // green
+        Ankle = 2,      // blue
+    };
+
+    public EPhysioRole physio_role;
+
+    /* generic/hmd
+     * LHR-A3EAE6FE
+     * 
+     * base station/lh_basestation_vive
+     * LHB-37F5B858
+     * LHB-988FEE1A
+     * 
+     * trackers htc}vr_tracker_vive_1_0
+     * LHR-1EA69A06
+     * LHR-1890594A
+     * 
+     * vive controllers vr_controller_vive_1_5
+     *  LHR-FFDB3D40
+     *  LHR-F7CFB44
+     */
+
+
+    public enum EIndex
+{
+    None = -1,
+    Hmd = (int)OpenVR.k_unTrackedDeviceIndex_Hmd,
+    Device1,
+    Device2,
+    Device3,
+    Device4,
+    Device5,
+    Device6,
+    Device7,
+    Device8,
+    Device9,
+    Device10,
+    Device11,
+    Device12,
+    Device13,
+    Device14,
+    Device15
+}
+
+public EIndex index;
+
+[Tooltip("If not set, relative to parent")]
+public Transform origin;
+
+    private EIndex getIndexForSerialNumber(string serial_to_find)
+    {
+        for (uint i = 0; i < kMaxDevicesToSearch; i++)
+        {
+            string serial_number = SteamVR.instance.GetStringProperty(ETrackedDeviceProperty.Prop_SerialNumber_String, i);
+            if (serial_number == serial_to_find)
+            {
+                return (EIndex)i;
+            }
+        }
+        return 0; 
+    }
+
+    // I want deterministic behaviour for the controllers - eg green maps to the knee.
+    public void Awake()
+    {
+        dumpDeviceProperties();
+        if (physio_role == EPhysioRole.Knee)
+        {
+            index = getIndexForSerialNumber(GreenTrackerSerial);
+            // assign the index for the Knee
+        }
+        else if (physio_role == EPhysioRole.Ankle)
+        {
+            index = getIndexForSerialNumber(BlueTrackerSerial);
+        }
+    }
 
     public bool isValid { get; private set; }
 
-	private void OnNewPoses(TrackedDevicePose_t[] poses)
-	{
-		if (index == EIndex.None)
-			return;
+private void dumpDeviceProperties()
+{
+    for (uint i = 0; i < kMaxDevicesToSearch; i++)
+    {
+        string serial_number = SteamVR.instance.GetStringProperty(ETrackedDeviceProperty.Prop_SerialNumber_String, i);
+        string render_model_name = SteamVR.instance.GetStringProperty(ETrackedDeviceProperty.Prop_RenderModelName_String, i);
+        Debug.Log(i + " SerialNumber: " + serial_number + " RenderModel: " + render_model_name);
+    }
+}
 
-		var i = (int)index;
+private void OnNewPoses(TrackedDevicePose_t[] poses)
+{
+    if (index == EIndex.None)
+        return;
 
-        isValid = false;
-		if (poses.Length <= i)
-			return;
+    var i = (int)index;
 
-		if (!poses[i].bDeviceIsConnected)
-			return;
+    //dumpDeviceProperties();
+    isValid = false;
+    if (poses.Length <= i)
+        return;
 
-		if (!poses[i].bPoseIsValid)
-			return;
+    if (!poses[i].bDeviceIsConnected)
+        return;
 
-        isValid = true;
+    if (!poses[i].bPoseIsValid)
+        return;
 
-		var pose = new SteamVR_Utils.RigidTransform(poses[i].mDeviceToAbsoluteTracking);
+    isValid = true;
 
-		if (origin != null)
-		{
-			transform.position = origin.transform.TransformPoint(pose.pos);
-			transform.rotation = origin.rotation * pose.rot;
-		}
-		else
-		{
-			transform.localPosition = pose.pos;
-			transform.localRotation = pose.rot;
-		}
-	}
+    var pose = new SteamVR_Utils.RigidTransform(poses[i].mDeviceToAbsoluteTracking);
 
-	SteamVR_Events.Action newPosesAction;
+    if (origin != null)
+    {
+        transform.position = origin.transform.TransformPoint(pose.pos);
+        transform.rotation = origin.rotation * pose.rot;
+    }
+    else
+    {
+        transform.localPosition = pose.pos;
+        transform.localRotation = pose.rot;
+    }
+}
 
-	SteamVR_TrackedObject()
-	{
-		newPosesAction = SteamVR_Events.NewPosesAction(OnNewPoses);
-	}
+SteamVR_Events.Action newPosesAction;
 
-	void OnEnable()
-	{
-		var render = SteamVR_Render.instance;
-		if (render == null)
-		{
-			enabled = false;
-			return;
-		}
+SteamVR_TrackedObject()
+{
+    newPosesAction = SteamVR_Events.NewPosesAction(OnNewPoses);
+    }
 
-		newPosesAction.enabled = true;
-	}
+    void OnEnable()
+{
+    var render = SteamVR_Render.instance;
+    if (render == null)
+    {
+        enabled = false;
+        return;
+    }
 
-	void OnDisable()
-	{
-		newPosesAction.enabled = false;
-		isValid = false;
-	}
+    newPosesAction.enabled = true;
+}
 
-	public void SetDeviceIndex(int index)
-	{
-		if (System.Enum.IsDefined(typeof(EIndex), index))
-			this.index = (EIndex)index;
-	}
+void OnDisable()
+{
+    newPosesAction.enabled = false;
+    isValid = false;
+}
+
+public void SetDeviceIndex(int index)
+{
+    if (System.Enum.IsDefined(typeof(EIndex), index))
+        this.index = (EIndex)index;
+}
 }
 
