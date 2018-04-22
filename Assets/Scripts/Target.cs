b2b11f0a7311dbfe4a9a67ea99c5daa6f1dbfa3e
Marisha <aethereal@gmail.com>
2018-04-22 12:31:51 -0700

    Added success noise

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -6,10 +6,12 @@ public class Target : MonoBehaviour {
 
     public Material onMaterial;
     public Material offMaterial;
+    public AudioSource successSound;
 
     void OnTriggerEnter(Collider other)
     {
         GetComponent<Renderer>().material = onMaterial;
+        successSound.Play();
     }
 
     void OnTriggerExit(Collider other)
