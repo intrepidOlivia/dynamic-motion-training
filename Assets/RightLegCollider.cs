7f2b1e0900fb6782341cf871d1615f67c27ed8b2
spayne <sean.d.payne@gmail.com>
2018-04-21 21:31:47 -0700

    Added right leg target

---
+++ Assets/RightLegCollider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightLegCollider : MonoBehaviour {
+
+    public Transform rightKnee;
+    public Transform rightFoot;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      transform.position = (rightKnee.position + rightFoot.position) / 2;
+	}
+}
