107c1cb9745bec143aa2bf4e040d2af1795622ea
Marisha <aethereal@gmail.com>
2018-04-22 12:37:05 -0700

    Added success noise to exercise scene

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -6,10 +6,12 @@ public class Target : MonoBehaviour {
 
     public Material onMaterial;
     public Material offMaterial;
+    public AudioSource successNoise;
 
     void OnTriggerEnter(Collider other)
     {
         GetComponent<Renderer>().material = onMaterial;
+        successNoise.Play();
     }
 
     void OnTriggerExit(Collider other)
