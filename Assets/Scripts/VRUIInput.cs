433d36c536e9d87a22e6454837c2c4634ac6afeb
spayne <sean.d.payne@gmail.com>
2018-04-22 15:10:11 -0700

    final demo tweaks

--- Assets/Scripts/VRUIInput.cs
+++ Assets/Scripts/VRUIInput.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using Valve.VR;
 
 [RequireComponent(typeof(SteamVR_LaserPointer))]
 public class VRUIInput : MonoBehaviour
 {
     private SteamVR_LaserPointer laserPointer;
     private SteamVR_TrackedController trackedController;
+    
 
     private void OnEnable()
     {
@@ -19,12 +21,25 @@ public class VRUIInput : MonoBehaviour
         trackedController = GetComponent<SteamVR_TrackedController>();
         if (trackedController == null)
         {
+            trackedController.controllerIndex = (uint)getIndexForSerialNumber("LHR-F7CF7B44");
             trackedController = GetComponentInParent<SteamVR_TrackedController>();
         }
         trackedController.TriggerClicked -= HandleTriggerClicked;
         trackedController.TriggerClicked += HandleTriggerClicked;
     }
 
+    private int getIndexForSerialNumber(string serial_to_find)
+    {
+        for (int i = 0; i < 12; i++)
+        {
+            string serial_number = SteamVR.instance.GetStringProperty(ETrackedDeviceProperty.Prop_SerialNumber_String, (uint)i);
+            if (serial_number == serial_to_find)
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
     private void HandleTriggerClicked(object sender, ClickedEventArgs e)
     {
         if (EventSystem.current.currentSelectedGameObject != null)
