054e5e4f02cf551f73e1c021dab154af0a1a6164
Marisha <aethereal@gmail.com>
2018-04-21 20:47:44 -0700

    Added decorative plants to scenes

---
+++ Assets/Scripts/introSceneSelect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class introSceneSelect : MonoBehaviour {
+
+    // public string nextScene;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void transitionTo(string nextScene) {
+        Initiate.Fade(nextScene, new Color(1, 1, 1), 1.0f);
+    }
+}
